05168898b6d48e8da95aaa3e2a67e2c9047d972c
George Kettleborough <kettleg@gmail.com>
2018-05-05 16:02:18 +0100

    Fix Editor doc strings

--- editor.py
+++ editor.py
@@ -36,17 +36,17 @@ class Editor():
         self.new_history = None
 
     def empty(self):
+        """Set the string to empty."""
         self.typed = deque()
         self.cursor = 0
 
     def set_string(self, string):
+        """Set the string to the given string."""
         self.typed = deque(string)
         self.cursor = len(self.typed)
 
     def edit_string(self, key, completions):
-        """Edit string and move cursor according to given key.  Return new string and
-    cursor.
-        """
+        """Edit string and move cursor according to given key."""
         if self.cursor > len(self.typed):
             self.cursor = len(self.typed)
         if len(key) == 1:
@@ -149,24 +149,30 @@ class Editor():
             return 0
 
     def has_more_history(self):
+        """Returns True if there is further history, otherwise False."""
         if len(self.history) > self.history_pos + 1:
             return True
         else:
             return False
 
     def to_fsarray(self, width, prompt, tail=""):
+        """Returns wrapped and coloured output as an fsarray. Includes the given
+prompt and tail and fits in the given width."""
         string = yellow(prompt) + fmtstr("".join(self.typed)) + tail
         chunks = [string[i:i+width] for i in range(0, len(string), width)]
         return fsarray(chunks)
 
     def to_string(self):
+        """Returns the contents as a string."""
         return "".join(self.typed)
 
     def cursor_pos(self, width, prompt):
+        """Gives the cursor position on the final line of the wrapped output."""
         cursor = self.cursor + len(prompt)
         return (cursor//width, cursor % width)
 
     def render_to(self, win, prompt="", tail=""):
+        """Renders the output to the given curtsies CursorAwareWindow."""
         fsa = self.to_fsarray(win.width, prompt, tail)
         cur = self.cursor_pos(win.width, prompt)
         win.render_to_terminal(fsa, cur)
